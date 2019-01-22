2969ddcfa674821301acecd12ac80d9670349d8e
mtking78 <mtking78@gmail.com>
2019-01-21 17:05:13 -0700

    Method to copy values from data instead of referencing them.

--- src/context.js
+++ src/context.js
@@ -8,8 +8,31 @@ const ProductContext = React.createContext();
 class ProductProvider extends Component {
 
   state = {
-    products: storeProducts,
+    // Line below will not work, because it is a reference to the data
+    // products: storeProducts,
+    products: [],
     detailProduct: detailProduct
+  }
+
+  componentDidMount() {
+    this.setProducts();
+  }
+
+  // Make a copy of the data to set the state.
+  setProducts = () => {
+    let tempProducts = [];
+    // Loop through the storeProducts array and do something with each item...
+    storeProducts.forEach(item => {
+      // copy the values for each item
+      const singleItem = {...item};
+      // get the old values from the products and add the newly created singleItem
+      tempProducts = [...tempProducts, singleItem];
+
+      // Get new set of the products values rather than references to them.
+      this.setState(() => {
+        return { products: tempProducts }
+      });
+    })
   }
 
   handleDetail = () => {
@@ -21,12 +44,13 @@ class ProductProvider extends Component {
 
   render() {
     return (
-      <ProductContext.Provider value={{
-        ...this.state,
-        handleDetail: this.handleDetail,
-        addToCart: this.addToCart
-
-      }}>
+      <ProductContext.Provider
+        value={{
+          ...this.state,
+          handleDetail: this.handleDetail,
+          addToCart: this.addToCart
+        }}
+      >
         {this.props.children}
       </ProductContext.Provider>
     )
