613c0da2984ca963a9eca8888e1db22010993055
mtking78 <mtking78@gmail.com>
2019-01-22 20:39:04 -0700

    Cart Component moved into a Cart Component file with CartColumns.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
 import { Switch, Route } from 'react-router-dom';
 import './App.css';
-import bootstrap from 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import Navbar from './components/Navbar';
 import ProductList from './components/ProductList';
 import Details from './components/Details';
+// Instead of proper path, select from folder using package.json file's "main" attribute
 import Cart from './components/Cart';
 import Default from './components/Default';
 import Modal from './components/Modal';
